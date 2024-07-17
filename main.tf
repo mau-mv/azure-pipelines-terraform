f5c89a29cf80bf80b7c95d957c5161df33784c2f
Mauricio <69824206+mau-mv@users.noreply.github.com>
2024-07-17 15:07:24 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ terraform {
     container_name         = "maumvterraformstate"
     key            = "terraform.tfstate"
   }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
 }
 
 provider "aws" {
@@ -50,6 +56,9 @@ provider "azurerm" {
   use_oidc = true
   skip_provider_registration = true
   features {}
+  client_id       = "6c5ba47e-5b54-4061-a18e-237d7e154d43"
+  tenant_id       = "a3439f07-fb51-4872-bf57-69ec8a73d1b7"
+  subscription_id = "3b69d161-00c7-4354-98eb-ebe3e520ef63"
 }
 
 resource "azurerm_virtual_network" "example-vnet" {
